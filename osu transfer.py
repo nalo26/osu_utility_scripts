a2a6568c84a651314dc0221add22fdf35bd9e947
nalo_ <37667081+nalo26@users.noreply.github.com>
2020-03-11 14:58:11 +0100

    Add files via upload

--- osu transfer.py
+++ osu transfer.py
@@ -2,8 +2,8 @@ import os
 import shutil
 import time
 
-src_path = "C:/Users/{user_name}/AppData/Local/osu!/Songs" # Change this to the SOURCE path of your beatmap folder
-dst_path = "F:/osu!/Songs" # Change this to the DESTINATION path of your beatmap folder
+src_path = "C:/Users/nalo_/AppData/Local/osu!/Songs" # Change this to the SOURCE path of your beatmap folder
+dst_path = "F:/osu!/Songs_test" # Change this to the DESTINATION path of your beatmap folder
 mode_to_keep = 3 # Change this to copy only beatmapts of this mode (std = 0, taiko = 1, catch = 2, mania = 3)
 
 start_time = time.time()
@@ -13,7 +13,7 @@ countError = 0
 
 for folder in os.listdir(src_path): # loop all mapsets
     transfer = False
-    if not(folder.endswith(".rar")):
+    try:
         print(f"({countFolder}) Looking for \"{folder}\"")
         for file in os.listdir(f"{src_path}/{folder}"): # loop all maps
             if file.endswith(".osu"):
@@ -34,5 +34,6 @@ for folder in os.listdir(src_path): # loop all mapsets
                             break
                 f.close()
             if transfer: break
+    except Exception: pass
 
 print(f"End, {countFolder} folders copied, {countError} error, took {round(time.time() - start_time, 5)} seconds")
